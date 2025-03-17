3efe8843542cceea15914bea71bc8cb974c6cb8d
Sangbed <83353411+Sang-19@users.noreply.github.com>
2025-03-17 15:37:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './styles/App.css';
 
 // Component imports
@@ -10,7 +9,7 @@ import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import LoginModal from './components/LoginModal';
-import Chat from './components/Chat.js'
+import Chat from './components/Chat';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,27 +26,47 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={
+      <AppContent openModal={openModal} isModalOpen={isModalOpen} closeModal={closeModal} />
+    </Router>
+  );
+}
+
+function AppContent({ openModal, isModalOpen, closeModal }) {
+  const location = useLocation();
+  const isChatPage = location.pathname === "/chat";
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="App">
+      {!isChatPage && <Navbar />} {/* Hide Navbar on /chat */}
+      
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
               <>
                 <Hero openModal={openModal} />
                 <Services />
                 <Contact />
               </>
-            } />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-        </main>
-        <Footer />
-        <LoginModal isOpen={isModalOpen} onClose={closeModal} />
-        
-      </div>
-    </Router>
+            }
+          />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </main>
+
+      {!isChatPage && <Footer />} {/* Hide Footer on /chat */}
+      
+      <LoginModal isOpen={isModalOpen} onClose={closeModal} />
+    </div>
   );
 }
 
